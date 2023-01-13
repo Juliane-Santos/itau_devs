4101d9768c50f54bbf58c2ff0099abd03f319b92
Juliane-Santos <juliane09santos2002@gmail.com>
2023-01-13 08:42:25 -0300

    Correção do programa anterior: Criado o teste da aula 3, que recebe um valor inteiro, caso receba o que pediu vai imprimir uma mensagem informando que aquele valor é positivo, caso não imprimirá outra finalizando o programa.

--- src/main/java/exercicios/aulasDaPlaylistDoProfessorIsidro/aula_3.java
+++ src/main/java/exercicios/aulasDaPlaylistDoProfessorIsidro/aula_3.java
@@ -1,17 +1,17 @@
 package exercicios.aulasDaPlaylistDoProfessorIsidro;
 
-/*O programa vai receber o valor, caso seja positivo ele vai imprimir uma mensagem informando isso ao usuário e caso não exibirá outra finalizando o programa*/
+/*O programa vai pedir um valor inteiro, caso receba o que pedio vai imprimir uma mensagem, caso não imprimirá uma outra finalizando o programa*/
 
 import java.util.Scanner;
 
-public class Aula_4 {
+public class aula_3 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int valor;
-        System.out.println("Digite um valor:");
+        System.out.println("Digite um valor inteiro:");
         valor = teclado.nextInt();
         if (valor >= 0) {
-            System.out.println("O valor " + valor + " é positivo.");
+System.out.println("O valor " + valor + " é positivo.");
         } else {
         System.out.println("Fim do programa."); }
     }
